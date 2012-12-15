e3df1facd73c9dd23c044db1981c1bdee33cb796
viking <smscash@hotmail.com>
2012-12-15 23:05:33 +0200

    billing 1st

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billing::Application.config.secret_token = 'fa666dabd8b89b62e2b99c18e59bce325d7382bd9699e15a3fe3ebbaff76ca80d9ce86365f701071e3bb396c5f3a4b08278fdd4e74557bf922e4a8e1a9552337'
